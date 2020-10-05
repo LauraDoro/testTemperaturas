8880f94e70dac416d88760af7d73f1c8bda97197
Laura <l.dominguez@inovalabs.es>
2020-10-05 10:12:22 +0200

    Cambio en dependencias

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,9 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './scss/app.scss';
+// shared_entry.js
+require('jquery');
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
